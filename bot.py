38513ebefea60fe7f615a122d35a1555d14de712
Mission Bit <missionbit@35a-MissionBit.local>
2020-11-16 16:53:24 -0800

    Module

--- bot.py
+++ bot.py
@@ -1,13 +1,48 @@
+# import discord
+# from discord.ext import commands
+
+# client = commands.Bot(command_prefix =  ';')
+
+# @client.event
+# async def on_ready():
+#     print("Bot is ready.")
+
 import discord
-from discord.ext import commands
+import random
+import asyncio
 
-client = commands.Bot(command_prefix =  '.')
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
 
-@client.event
-async def on_ready():
-    print("Bot is ready.")
-    
-client.run("Nzc1NTI5MTA0Njg1OTI0MzUz.X6npww.AnFpJ1dV6lGYjM95EE_WR_bO38Q")
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('$guess'):
+            await message.channel.send('Guess a number between 1 and 10.')
+
+            def is_correct(m):
+                return m.author == message.author and m.content.isdigit()
+
+            answer = random.randint(1, 10)
+
+            try:
+                guess = await self.wait_for('message', check=is_correct, timeout=5.0)
+            except asyncio.TimeoutError:
+                return await message.channel.send('Sorry, you took too long it was {}.'.format(answer))
+
+            if int(guess.content) == answer:
+                await message.channel.send('You are right!')
+            else:
+                await message.channel.send('Oops. It is actually {}.'.format(answer))
+
+client = MyClient()
+client.run("Nzc1NTI5MTA0Njg1OTI0MzUz.X6npww.2uRVAFXRktoM1HrbGN6pZPubmIw")
 
 
 
